080560bde5a804b34ff16011967385851ddcd9b6
Thomas Swann <tkswann@mac.com>
2016-06-14 21:52:25 -0500

    tweaked user interaction, added message styles

--- js/chatEvents.js
+++ js/chatEvents.js
@@ -42,9 +42,10 @@ var Chatty = (function (oldChatty) {
 							outputUl.appendChild(newMsg);		
 				}
 	};
-
+// parse new message from user input
 	oldChatty.parseMessage = function (element, userInput) {
 		 var newMsg;
+		// create elements for message string
 		 var msg = userInput,
 		 		message = document.createElement('li'),
 		 		messageUserName = document.createElement('p'),
@@ -53,10 +54,10 @@ var Chatty = (function (oldChatty) {
 		 		messageDelete = document.createElement('button'),
 				messageEdit = document.createElement('button'),
 				timeStamp = new Date().toString();
-
+			// assign unique id to each new message created
 				message.id = counter;
 				counter++;
-
+			// set content/classes for all message string DOM elements
 				messageUserName.innerHTML = `WhiteDragon::&nbsp;&nbsp`;
 				messageUserName.classList.add('username');
 
@@ -71,29 +72,40 @@ var Chatty = (function (oldChatty) {
 				messageDelete.innerText = "X";
 				messageDelete.classList.add("delete");
 
+			// append elements to parent list item
 				message.appendChild(messageUserName);
 				message.appendChild(messageText);
 				message.appendChild(messageDelete);
 				message.appendChild(messageEdit);
 				message.appendChild(messageDate);
 				newMsg = message;
-				element.appendChild(newMsg);	
+			// append new message to message output
+				element.appendChild(newMsg);
 
 				userMessages.push(newMsg);
+			// reset input value to nill
 				inputEl.value = '';
 	};
-
+// return private array of user messages
 	oldChatty.getUserMessages = function () {
 		return userMessages;
 	};
 
-
+// add event listeners to DOM elements
 	oldChatty.addEvents = function () {
+
 		inputEl.addEventListener('keydown', enterListener);
+
 		clearButton.addEventListener('click', clearMessages);
+
 		document.querySelector("body").addEventListener("click", function () {
+			// disable clear button after last message is deleted
+			if (outputUl.children.length === 1) {
+			clearButton.setAttribute('disabled', 'true');
+			}
 			index = event.target.parentNode.id;
 			Chatty.deleteEvents(event, index);
+			Chatty.editEvents(event);
 		});
 	};
 
--- js/deleteEvents.js
+++ js/deleteEvents.js
@@ -2,36 +2,46 @@ var Chatty = (function (oldChatty) {
 	var	oldContent;
 
 	oldChatty.deleteEvents = function(event, index) {
-	 		 console.log(event);
-			
-	 		 // Handle the click event on any li
-	 		 if (event.target.tagName.toLowerCase() === "li") {
-	 		 }
-			
-	 		 // Handle the click event on any DOM element with a certain class
+			 // Handle the click event on any DOM element with a certain class
 	 		 if (event.target.className === "delete") {
+	 		 	// remove deleted message from private message array
 	 		 	 Chatty.getUserMessages().splice(index, 1);
+	 		 	 // remove selected message with delete button
 	 		   event.target.parentNode.parentNode.removeChild(event.target.parentNode);
 	 		   
 	 		 }
-			
-	 		 if (event.target.className === "edit") {
+		};
+
+		oldChatty.editEvents = function (event) {
+			if (event.target.className === "edit") {
+	 		 	// save old message content 
 	 		   oldContent = event.path[1].children[1].innerText;
+	 		   // add input box to innerHTML of clicked message
 	 		   event.path[1].children[1].innerHTML = `<input type="text" id="editBox" />`;
-	 		   event.path[1].children[1].children[0].value = oldContent;
+	 		   // add messge content into edit input for better user interaction
+	 		  var editInput =  event.path[1].children[1].children[0];
+	 		  		editInput.value = oldContent;
+	 		  		// editInput.select();
+	 		   // diable edit button while in edit mode
+	 		   event.target.setAttribute('disabled', 'true');
 	 		 }
 
 	 		 // Inspect the `id` property of the event target
 	 		 if (event.target.id === "editBox") {
 	 		   event.target.addEventListener('keydown', function (e) {
 	 		   		 if (e.keyCode === 13 && event.target.value !== '') {
+	 		   		 	// enable edit button after user is done
+	 		   		  event.path[1].nextSibling.nextSibling.removeAttribute('disabled');	
+	 		   		  // set message value to user input from edit box
 	 		   			event.path[2].children[1].innerText = event.target.value;
 	 		   		} else if (e.keyCode === 13 && event.target.value === '') {
+	 		   		  // enable edit button after user is done
+	 		   		  event.path[1].nextSibling.nextSibling.removeAttribute('disabled');
+	 		   		  // if nothing changed, set the content to the old message
 	 		   			event.path[2].children[1].innerText = oldContent;
 	 		   		}
 	 		   });
-	 				
-	 		 }
+	 		  }
 		};
 
 
